c0b3838d15922389fa658dc71ba0fe3cac2d6686
Long Nguyen <ba198647@gmail.com>
2016-06-21 00:29:16 +0800

    up

--- Localization.py
+++ Localization.py
@@ -6,14 +6,18 @@ m=np.array([0.6,0.6,0.2,0.2,0.2])
 #Enter code here
 p = p * m
 p = p/p.sum()
-# print p
-
+print p
+print "--------------------------------"
 p=[0.2, 0.2, 0.2, 0.2, 0.2]
 world=['green', 'red', 'red', 'green', 'green']
 measurements = ['red', 'green']
+motions = [1,1]
 Z = 'red'
 pHit = 0.6
 pMiss = 0.2
+pExact = 0.8
+pOvershoot = 0.1
+pUndershoot = 0.1
 
 def sense(p, Z):
     q=[]
@@ -25,17 +29,26 @@ def sense(p, Z):
         q[i] = q[i]/s
     return q
 
-# for i in range(len(measurements)):
-#     p = sense(p,measurements[i])
-#     print p
+for i in range(len(measurements)):
+    p = sense(p,measurements[i])
+    print p
+print "--------------------------------"
+
 p=[0, 1, 0, 0, 0]
 def move(p, U):
     q = []
     for i in range(len(p)):
-        # step = (i-U)%len(p)
-        # q.append(p[step])
-        U = U % len(p)
-        q = p[-U:] + p[:-U]
+        s = pExact * p[(i-U)%len(p)] + pOvershoot * p[(i-U-1)%len(p)] + pUndershoot * p[(i-U+1)%len(p)]
+        q.append(s)
     return q
 
-print move(p, -1)
+for i in range(1000):
+    p = move(p, 1)
+print p
+print "--------------------------------"
+
+for i in range(len(measurements)):
+    p = sense(p,measurements[i])
+    p = move(p,motions[i])
+print p
+print "--------------------------------"
