e3ce605fb849428df4e8d421660f979568cbb2ea
Long Nguyen <ba198647@gmail.com>
2016-06-25 18:16:55 +0800

    u

--- Search/DynamicProgramming.py
+++ Search/DynamicProgramming.py
@@ -25,36 +25,34 @@ delta = [[-1, 0],  # go up
 delta_name = ['^', '<', 'v', '>']
 
 
-def compute_value(grid, goal, cost):
-    value = [[99 for col in range(len(grid[0]))] for row in range(len(grid))]
+def compute_value(grid,goal,cost):
+    value = [[99 for row in range(len(grid[0]))] for col in range(len(grid))]
     change = True
+
     while change:
+        change = False
+
         for x in range(len(grid)):
             for y in range(len(grid[0])):
                 if goal[0] == x and goal[1] == y:
                     if value[x][y] > 0:
                         value[x][y] = 0
-                        change = True
-                elif grid[x][y] == 0:
-                    for i in range(len(delta)):
-                        x2 = x + delta[i][0]
-                        y2 = y + delta[i][1]
 
-                        if x2 >=0 and x2 < len(grid) and y2 >= 0 and y2 < len(grid[0]) and grid[x2][y2] == 0:
+                        change = True
+
+                elif grid[x][y] == 0:
+                    for a in range(len(delta)):
+                        x2 = x + delta[a][0]
+                        y2 = y + delta[a][1]
+
+                        if x2 >= 0 and x2 < len(grid) and y2 >= 0 and y2 < len(grid[0]) and grid[x2][y2] == 0:
                             v2 = value[x2][y2] + cost
 
                             if v2 < value[x][y]:
                                 change = True
                                 value[x][y] = v2
-
-
-    # ----------------------------------------
-    # insert code below
-    # ----------------------------------------
-
-    # make sure your function returns a grid of values as
-    # demonstrated in the previous video.
     return value
+
 value = compute_value(grid, goal, cost)
 for i in range(len(value)):
     print value[i]
